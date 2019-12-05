7b4468be4e895650fd51f4862887c6866827e4a9
Gerax Sotelo <gsotelo23@gmail.com>
2019-12-05 15:47:14 -0700

    Complete search function and create template

--- app/Models/Gif.js
+++ app/Models/Gif.js
@@ -1,9 +1,17 @@
 export default class Gif {
     constructor(data) {
         this.title = data.title
+        this.url = data.image_mp4_url || data.url
     }
 
     get Template() {
-        return this.title
+        return `
+            <div class="col-6 col-md-3">
+                <video autoplay>
+                    <source
+                    src="https://media0.giphy.com/media/MrxXXBriEIKBO/giphy.mp4?cid=7c72dc3d960d4b20fe4e00de81f67f6b9f11253856b96c8b&rid=giphy.mp4">
+                </video>
+            </div>
+        `
     }
 }
--- app/Services/GifsService.js
+++ app/Services/GifsService.js
@@ -1,4 +1,5 @@
 import store from "../store.js";
+import Gif from "../Models/Gif.js";
 
 let _sandbox = axios.create({
   baseURL: "http://bcw-sandbox.herokuapp.com/api/geraxsotelo/gifs"
@@ -10,8 +11,10 @@ let _giphyApi = axios.create({
 class GifsService {
   async searchAsync(query) {
     let res = await _giphyApi.get("search?api_key=9Se48L43XQqf3fZEUQfKPN5RKdGqrRLY&rating=pg&limit=10&q=" + query)
-    res.data.data
+    console.log("res ", res);
 
+    let results = res.data.data.map(g => new Gif(g))
+    store.commit("gifs", results)
   }
 }
 
